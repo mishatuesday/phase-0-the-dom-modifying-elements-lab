86792aba64c5ffca3288ea86829e2426d0eb79ad
Misha Tuesday <mishatuesday@gmail.com>
2022-09-20 09:43:24 -0400

    commit tree 757b0240dfd9e504a82b129ad10f139b262094db

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainn = document.getElementById("main")
+mainn.remove()
+
+const newHeader = document.getElementsByTagName("h1")[0]
+newHeader.textContent = "is the champion."
